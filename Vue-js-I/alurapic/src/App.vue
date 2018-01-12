3078f7d7eed4d8367cc677eda577a19537773f53
Willian Schenkel <hischenkel@gmail.com>
2018-01-12 00:46:26 -0200

    Vuejs

--- Vue-js-I/alurapic/src/App.vue
+++ Vue-js-I/alurapic/src/App.vue
@@ -1,32 +1,86 @@
 <template>
-  <div>
-    <h1>{{ titulo }}</h1>
-    <ul>
-      <li v-for="foto of fotos">
-        <img :src="foto.url" :alt="foto.titulo">
+  <div class="corpo">
+
+    <h1 class="centralizado">{{ titulo }}</h1>
+    <input type="search" class="filtro" @input="filtro = $event.target.value" placeholder="Filtre por nome">
+
+    <ul class="lista-fotos">
+      <li class="lista-fotos-item" v-for="foto of fotosComFiltro">
+        <meu-painel :titulo="foto.titulo">
+          <imagem-responsiva :url="foto.url" :titulo="foto.titulo" />
+        </meu-painel>
       </li>
     </ul>
   </div>
 </template>
 
 <script>
+
+  import Painel from './components/shared/painel/Painel.vue';
+  import ImagemResponsiva from './components/shared/imagem-responsiva/ImagemResponsiva.vue';
+
   export default {
+
+    components: {
+      'meu-painel': Painel,
+      'imagem-responsiva': ImagemResponsiva
+    },
 
     data() {
       return {
         titulo: 'Alurapic',
-        fotos: []
+        fotos: [],
+        filtro: ''
       }
     },
 
+    computed: {
+      fotosComFiltro() {
+        if (this.filtro) {
+
+          let regex = new RegExp(this.filtro.trim(), "i");
+          return this.fotos.filter(foto => regex.test(foto.titulo));
+
+        } else {
+          return this.fotos;
+        }
+      }
+
+    },
     created() {
           this.$http.get("http://localhost:3000/v1/fotos")
             .then(res => res.json())
             .then(fotos => this.fotos = fotos, err => console.log(err));
     }
-
   }
 </script>
 
 <style>
+
+  .centralizado {
+    text-align: center;
+  }
+
+  .corpo {
+    font-family: Helvetica, sans-serif;
+    margin: 0 auto;
+    width: 96%;
+  }
+
+  .lista-fotos {
+    list-style: none;
+  }
+
+  .lista-fotos .lista-fotos-item {
+    display: inline-block;
+  }
+
+  .imagem-responsiva {
+    width: 100%;
+  }
+
+  .filtro {
+    width: 100%;
+  }
+
 </style>
---
+++ Vue-js-I/alurapic/src/components/shared/painel/Painel.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="painel">
+    <h2 class="painel-titulo" @dblclick="visible = !visible">{{ titulo }}</h2>
+
+    <transition name="painel-fade">
+      <div class="painel-corpo" v-show="visible">
+        <slot></slot>
+      </div>
+    </transition>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['titulo'],
+
+    data() {
+      return {
+        visible: false
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+  .centralizado {
+    text-align: center;
+  }
+
+  .corpo {
+    font-family: Helvetica, sans-serif;
+    margin: 0 auto;
+    width: 96%;
+  }
+
+  .lista-fotos {
+    list-style: none;
+  }
+
+  .lista-fotos .lista-fotos-item {
+    display: inline-block;
+  }
+
+  /* estilo do painel */
+   .painel {
+    padding: 0 auto;
+    border: solid 2px grey;
+    display: inline-block;
+    margin: 5px;
+    box-shadow: 5px 5px 10px grey;
+    width: 200px;
+    height: 100%;
+    vertical-align: top;
+    text-align: center;
+  }
+
+  .painel .painel-titulo {
+    text-align: center;
+    border: solid 2px;
+    background: lightblue;
+    margin: 0 0 15px 0;
+    padding: 10px;
+    text-transform: uppercase;
+  }
+
+  * {
+    box-shadow: 5px 5px 5px;
+  }
+
+  .painel-fade-enter, .painel-fade-leave-active {
+    opacity: 0
+  }
+
+  .painel-fade-enter-active, .painel-fade-leave-active {
+    transition: opacity .4s
+  }
+</style>
+
+/*
+painel-fade-enter // antes do elemento ser incluído ou removido, o estado atual
+painel-fade-enter-active // quando o elemento esta sendo incluído
+painel-fade-leave-active // quando o elemento esta sendo removido
+*/
